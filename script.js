ac507a0ccc550ef84519e615c0ad4394ec5bf214
DipeshGoud <112858278+DipeshGoud@users.noreply.github.com>
2023-05-20 22:04:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+
+// let option = document.getElementById("colorSelect").value;
+
+// function removeSelect(option) {
+// 	let select = document.getElementById("colorSelect");
+// 	select.remove(option);
+// }
+
+// document.getElementById("selectButton").addEventListener("click", removeSelect(option));
+
+
+document.getElementById("selectButton").addEventListener("click", removeSelect);
+
+function removeSelect() {
+  let select = document.getElementById("colorSelect");
+  let selectedValue = select.value;
+
+  if (selectedValue !== "") {
+    let options = select.options;
+    for (let i = 0; i < options.length; i++) {
+      if (options[i].value === selectedValue) {
+        select.remove(i);
+        break;
+      }
+    }
+  }
+}
